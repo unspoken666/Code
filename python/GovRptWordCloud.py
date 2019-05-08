9861e2a62bf2711c6eff8fac9a06e6394baef897
unspoken666 <906462762@qq.com>
2019-05-08 10:04:22 +0800

    government working report

---
+++ python/GovRptWordCloud.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#GovRptWordCloudv1.py
+import jieba,wordcloud
+f = open("新时代中国特色社会主义.txt","r",encoding="utf-8")
+t = f.read()
+f.close()
+ls = jieba.lcut(t)
+txt = " ".join(ls)
+w = wordcloud.WordCloud(font_path = "msyh.ttc",    width = 1000,height = 700,background_color = "white",max_words = 15)
+w.generate(txt)
+w.to_file("grwordcloud.png")
+
+
+#GovRptWordCloudv1.py
+import jieba,wordcloud
+f = open("关于实施乡村振兴战略的意见.txt","r",encoding="utf-8")
+t = f.read()
+f.close()
+ls = jieba.lcut(t)
+txt = " ".join(ls)
+w = wordcloud.WordCloud(font_path = "msyh.ttc",    width = 1000,height = 700,background_color = "white",max_words = 15)
+w.generate(txt)
+w.to_file("grwordcloud.png")
+
