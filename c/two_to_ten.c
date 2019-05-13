af4d298593cb9dc841ace9af8a237f9f0d5c9d63
unspoken666 <906462762@qq.com>
2019-05-13 09:09:20 +0800

    Convert the input binary number to a decimal number

---
+++ c/two_to_ten.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+int TwoToTen(char * str2)
+{
+	int len = strlen(str2);
+	int i = 0;
+	int dec = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (str2[i] == '1')
+		{
+			dec += pow(2, len-i-1);
+		}
+	}
+	return dec;
+}
+int main()
+{
+	char str2[101] = "";
+	gets_s(str2,100);
+	int ret = TwoToTen(str2);
+	printf("%d\n", ret);
+	getchar();
+	return 0;
+}
